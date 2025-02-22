b717a8657de0061d3c396899f968a506deea1c47
shajmil-ufs <shajmil@ufstechnologies.com>
2025-02-22 23:35:18 +0530

    new

--- src/services/preview.service.js
+++ src/services/preview.service.js
@@ -140,13 +140,7 @@ const fetchWithFallback = async(url) => {
                 request.abort();
             }
         });
-        await page.setExtraHTTPHeaders({
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Upgrade-Insecure-Requests': '1',
-            'Connection': 'keep-alive'
-        });
+        
 
         await Promise.all([
             page.setViewport({ width: 1280, height: 720, deviceScaleFactor: 1 }),
